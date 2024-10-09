87d111ec43c9a1bf7c15d32081ef7298bcaa42d5
Ryan Kuederle <ryan@rkuederle.com>
2024-10-09 11:44:15 -0400

    Restored directory naming

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ else
     echo "Docker is already installed."
 fi
 
-sudo usermod -aG docker $USER
+sudo usermod -aG docker 
 
-git clone https://github.com/kuederleR/ros2_app_deplopyment.git /tmp/ros2-deployment
-cd /tmp/ros2-deployment
+git clone https://github.com/kuederleR/ros2_app_deplopyment.git /tmp/ros2_app_deployment
+cd /tmp/ros2_app_deployment
 sudo bash deploy/install.sh
 cd ..
-sudo rm -rf /tmp/ros2-deployment
+sudo rm -rf /tmp/ros2_app_deployment
