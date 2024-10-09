6acdc9f6870bc0409ad79537278f47e862411a12
Ryan Kuederle <ryan@rkuederle.com>
2024-10-09 11:41:49 -0400

    Some changes

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/kuederleR/ros2_app_deplopyment.git /tmp/ros2_app_deployment
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing Docker..."
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+else
+    echo "Docker is already installed."
+fi
+
+sudo usermod -aG docker $USER
+
+git clone https://github.com/kuederleR/ros2_app_deplopyment.git /tmp/ros2-deployment
 cd /tmp/ros2_app_deployment
 sudo bash deploy/install.sh
 cd ..
-sudo rm -rf /tmp/ros2_app_deployment
+sudo rm -rf /tmp/ros2-deployment
